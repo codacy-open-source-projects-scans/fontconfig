2d8ccd56468cd437e9abcf248e46ad90f80d77f2
Codacy Archive <codacy-archive@local>
2025-12-15 09:33:07 +0000

    snapshot 2025-12-15T09:33:07.369360+00:00

--- src/fcdir.c
+++ src/fcdir.c
@@ -32,7 +32,9 @@
 #  include "fontconfig/fcfontations.h"
 #endif
 
+#if ENABLE_FREETYPE
 #include "fontconfig/fcfreetype.h"
+#endif
 
 FcBool
 FcFileIsDir (const FcChar8 *file)
@@ -83,7 +85,13 @@ FcFileScanFontConfig (FcFontSet     *set,
 	fflush (stdout);
     }
 
-    unsigned int (*query_function) (const FcChar8 *, unsigned int, FcBlanks *, int *, FcFontSet *) = FcFreeTypeQueryAll;
+    unsigned int (*query_function) (const FcChar8 *, unsigned int, FcBlanks *, int *, FcFontSet *) =
+#if ENABLE_FONTATIONS && !defined(ENABLE_FREETYPE)
+    FcFontationsQueryAll;
+#elif ENABLE_FREETYPE
+    FcFreeTypeQueryAll;
+#endif
+
 #if ENABLE_FONTATIONS
     if (getenv ("FC_FONTATIONS")) {
 	query_function = FcFontationsQueryAll;
